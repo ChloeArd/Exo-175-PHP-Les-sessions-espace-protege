5d79657e2d99dc968dce8dcebd9b6d5ce5313436
ChloeArd <chloe.ardoise@gmail.com>
2021-02-03 13:49:15 +0100

    Exercice 175

--- index.php
+++ index.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>Sessions espace protégé</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<form method="post" action="protect.php">
-    <div>
+<?php
+    if (isset($_GET["e"])) { ?>
+        <div class="error">Ce n'est pas le bon, mot de passe !</div>
+<?php
+    }
+    ?>
+<div id="container">
+    <form method="post" action="protect.php">
         <label for="password">Entrez le bon mot de passe : </label>
         <input type="password" name="password" id="password" required>
-    </div>
-    <input type="submit" value="Envoyer">
-</form>
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
+
 
 </body>
 </html>
 
-<?php
-    if (isset($_GET["e"])){
-        echo "Ce n'est pas le bon, mot de passe !";
-    }
+
